a645a45f55bcf5ab9522e391fa5977078bc51d19
Dark_Benky <matus.benky@gmail.com>
2024-05-02 20:56:55 +0200

    Implement image blurring and contrast adjustment functions in mask_gen.pyx

--- mask_gen.pyx
+++ mask_gen.pyx
@@ -29,6 +29,171 @@ cpdef blur_cython(np.ndarray[np.uint8_t, ndim=3] color_layer , int radius):
 
     return color_layer
 
+# TODO: Implement the following functions in Python
+
+cpdef blur_circle_cython(np.ndarray[np.uint8_t, ndim=3] color_layer, int radius):
+    cdef int i, j, sum_r, sum_g, sum_b, count = 0, x, y
+    cdef int r_squared = <int>pow(radius, 2)
+    cdef int lenght = color_layer.shape[0]
+    cdef int width = color_layer.shape[1]
+
+    for i in range(lenght):
+        for j in range(width):
+            if pow(i, 2) + pow(j, 2) < r_squared:
+                sum_r = 0
+                sum_g = 0
+                sum_b = 0
+                count = 0
+                for x in range(-radius, radius):
+                    for y in range(-radius, radius):
+                         if 0 <= i + x < lenght and 0 <= j + y < width:
+                            sum_r += color_layer[i + x, j + y, 0]
+                            sum_g += color_layer[i + x, j + y, 1]
+                            sum_b += color_layer[i + x, j + y, 2]
+                            count += 1
+                color_layer[i, j, 0] = sum_r // count
+                color_layer[i, j, 1] = sum_g // count
+                color_layer[i, j, 2] = sum_b // count
+
+    return color_layer
+
+# TODO: Implement the following functions in Python
+
+cpdef increase_contrast_cython(np.ndarray[np.uint8_t, ndim=3] color_layer, int radius, float factor):
+    cdef int i, j, sum_r, sum_g, sum_b, count
+    cdef int length = color_layer.shape[0]
+    cdef int width = color_layer.shape[1]
+    cdef float contrast_factor
+
+    for i in range(length):
+        for j in range(width):
+            sum_r = 0
+            sum_g = 0
+            sum_b = 0
+            count = 0
+            for x in range(-radius, radius):
+                for y in range(-radius, radius):
+                    if 0 <= i + x < length and 0 <= j + y < width:
+                        sum_r += color_layer[i + x, j + y, 0]
+                        sum_g += color_layer[i + x, j + y, 1]
+                        sum_b += color_layer[i + x, j + y, 2]
+                        count += 1
+            avg_r = sum_r // count
+            avg_g = sum_g // count
+            avg_b = sum_b // count
+            # Increase contrast
+            contrast_factor = 1.0 + factor
+            color_layer[i, j, 0] = max(0, min(255, int((avg_r - 128) * contrast_factor + 128)))
+            color_layer[i, j, 1] = max(0, min(255, int((avg_g - 128) * contrast_factor + 128)))
+            color_layer[i, j, 2] = max(0, min(255, int((avg_b - 128) * contrast_factor + 128)))
+
+    return color_layer
+
+# TODO: Implement the following functions in Python
+
+cpdef increase_contrast_circle_cython(np.ndarray[np.uint8_t, ndim=3] color_layer, int radius, float factor):
+    cdef int i, j, sum_r, sum_g, sum_b, count
+    cdef int length = color_layer.shape[0]
+    cdef int width = color_layer.shape[1]
+    cdef float contrast_factor
+    cdef int r_squared = <int>pow(radius, 2)
+
+    for i in range(length):
+        for j in range(width):
+            if pow(i, 2) + pow(j, 2) < r_squared:
+                sum_r = 0
+                sum_g = 0
+                sum_b = 0
+                count = 0
+                for x in range(-radius, radius):
+                    for y in range(-radius, radius):
+                        if 0 <= i + x < length and 0 <= j + y < width:
+                            sum_r += color_layer[i + x, j + y, 0]
+                            sum_g += color_layer[i + x, j + y, 1]
+                            sum_b += color_layer[i + x, j + y, 2]
+                            count += 1
+                avg_r = sum_r // count
+                avg_g = sum_g // count
+                avg_b = sum_b // count
+                # Increase contrast
+                contrast_factor = 1.0 + factor
+                color_layer[i, j, 0] = max(0, min(255, int((avg_r - 128) * contrast_factor + 128)))
+                color_layer[i, j, 1] = max(0, min(255, int((avg_g - 128) * contrast_factor + 128)))
+                color_layer[i, j, 2] = max(0, min(255, int((avg_b - 128) * contrast_factor + 128)))
+
+    return color_layer
+
+
+# TODO: Implement the following functions in Python
+
+cpdef decrease_contrast_cython(np.ndarray[np.uint8_t, ndim=3] color_layer, int radius, float factor):
+    cdef int i, j, sum_r, sum_g, sum_b, count
+    cdef int length = color_layer.shape[0]
+    cdef int width = color_layer.shape[1]
+    cdef float contrast_factor
+    
+
+    for i in range(length):
+        for j in range(width):
+            sum_r = 0
+            sum_g = 0
+            sum_b = 0
+            count = 0
+            for x in range(-radius, radius):
+                for y in range(-radius, radius):
+                    if 0 <= i + x < length and 0 <= j + y < width:
+                        sum_r += color_layer[i + x, j + y, 0]
+                        sum_g += color_layer[i + x, j + y, 1]
+                        sum_b += color_layer[i + x, j + y, 2]
+                        count += 1
+            avg_r = sum_r // count
+            avg_g = sum_g // count
+            avg_b = sum_b // count
+            # Decrease contrast
+            contrast_factor = 1.0 - factor
+            color_layer[i, j, 0] = max(0, min(255, int((avg_r - 128) * contrast_factor + 128)))
+            color_layer[i, j, 1] = max(0, min(255, int((avg_g - 128) * contrast_factor + 128)))
+            color_layer[i, j, 2] = max(0, min(255, int((avg_b - 128) * contrast_factor + 128)))
+
+    return color_layer
+
+
+# TODO: Implement the following functions in Python
+
+cpdef decrease_contrast_circle_cython(np.ndarray[np.uint8_t, ndim=3] color_layer, int radius, float factor):
+    cdef int i, j, sum_r, sum_g, sum_b, count
+    cdef int length = color_layer.shape[0]
+    cdef int width = color_layer.shape[1]
+    cdef float contrast_factor
+    cdef int r_squared = <int>pow(radius, 2)
+
+    for i in range(length):
+        for j in range(width):
+            if pow(i, 2) + pow(j, 2) < r_squared:
+                sum_r = 0
+                sum_g = 0
+                sum_b = 0
+                count = 0
+                for x in range(-radius, radius):
+                    for y in range(-radius, radius):
+                        if 0 <= i + x < length and 0 <= j + y < width:
+                            sum_r += color_layer[i + x, j + y, 0]
+                            sum_g += color_layer[i + x, j + y, 1]
+                            sum_b += color_layer[i + x, j + y, 2]
+                            count += 1
+                avg_r = sum_r // count
+                avg_g = sum_g // count
+                avg_b = sum_b // count
+                # Decrease contrast
+                contrast_factor = 1.0 - factor
+                color_layer[i, j, 0] = max(0, min(255, int((avg_r - 128) * contrast_factor + 128)))
+                color_layer[i, j, 1] = max(0, min(255, int((avg_g - 128) * contrast_factor + 128)))
+                color_layer[i, j, 2] = max(0, min(255, int((avg_b - 128) * contrast_factor + 128)))
+
+    return color_layer
+
+
+
 cpdef remove_cython(np.ndarray[np.npy_bool, ndim=2] layers,
                           np.ndarray[np.uint8_t, ndim=3] colors,
                           np.ndarray[np.npy_bool, ndim=2] mask):
